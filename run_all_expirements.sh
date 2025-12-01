df57e32bbcc7176c851ca155ba916b48eadbb194
zakcory <93665026+zakcory@users.noreply.github.com>
2025-12-01 22:22:58 +0200

    Update run_all_expirements.sh

--- run_all_expirements.sh
+++ run_all_expirements.sh
@@ -2,7 +2,8 @@
 export CUBLAS_WORKSPACE_CONFIG=:4096:8
 export CUDNN_DETERMINISTIC=1
 
-conda activate cs236207
+source .venv/bin/activate
 
-# python pipeline.py --model_name lstm --dataset_name IMDB --fine_tune
+python pipeline.py --model_name beannet --dataset_name drybean --fine_tune
+python pipeline.py --model_name lstm --dataset_name IMDB --fine_tune
 python pipeline.py --model_name  resnet18 --dataset_name cifar10 --fine_tune
